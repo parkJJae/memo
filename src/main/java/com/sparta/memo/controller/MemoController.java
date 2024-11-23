f2df718d3f8494f6ae17e92d0d1f80f18d6e2c85
parkJJae <wgpark0830@naver.com>
2024-11-23 22:37:03 +0900

    Create, Read, Update, Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -45,4 +45,33 @@ public class MemoController {
 
             return responseList;
         }
+
+        @PutMapping("/memos/{id}")
+        public long updateMemo(@PathVariable long id, @RequestBody MemoRequestDto requestDto){
+            // memo가 데베에 존재하는지 확인
+            if(memoList.containsKey(id)){
+                // 해당 메모 가져오기
+                Memo memo = memoList.get(id);
+
+                // 메모 수정
+                memo.update(requestDto);
+                return memo.getId();
+            } else {
+                throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+            }
+
+        }
+
+        @DeleteMapping("/memos/{id}")
+        public long deleteMemo(@PathVariable long id){
+            // 해당 메모가 데이터베이스에 존재하는지
+            if(memoList.containsKey(id)){
+                memoList.remove(id);
+                return id;
+            }
+            else {
+                throw new IllegalArgumentException("메모 없음");
+            }
+        }
+
 }
